868ecde0254e33e5015edaefe6a310330348778d
Stawen DRONEK <stanislas.draunet@gmail.com>
2015-11-30 16:36:11 +0000

    add date on changelog

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	    for(var i = 0; i < 5; i++){
             $('#version').append(
                 '<div class="panel panel-default"> \
-    			    <div class="panel-heading">' + keys[i] + '</div> \
+    			    <div class="panel-heading">' + keys[i] + ' <br/>' + roadmap[keys[i]].date + '</div> \
     			    <div class="panel-body"> ' + roadmap[keys[i]].changelog + ' </div> \
     			 </div>');
         }
